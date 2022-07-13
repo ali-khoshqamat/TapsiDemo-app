c43b5e665950da53c4570d7eb3c22b417db7b793
ali-khoshqamat <ali_khoshghamat@yahoo.com>
2022-07-13 23:11:14 +0430

    create Tapsi-Demo app

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav");
+
+toggler.addEventListener("click", (e) => {
+  navbar.classList.toggle("nav__expanded");
+});
+
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    const targeTabContent = document.querySelector(
+      `#${tab.dataset.tabTarget}`
+    );
+
+    // REMOVE PREVIOUS ACTIVED CLASSES
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tabContents.forEach((tabContents) =>
+      tabContents.classList.remove("active")
+    );
+
+    // ADD NEW ACTIVE CLASSES
+    tab.classList.add("active");
+    targeTabContent.classList.add("active");
+  });
+});
